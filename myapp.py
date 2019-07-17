91e1515336d90342cba2c7261c66c620cebf3b32
mikelildaps <mikelildaps100@gmail.com>
2019-07-17 17:20:16 +0100

    today is webdev

---
+++ myapp.py
@@ -0,0 +1,28 @@
+#----------------0. imports-----------------
+
+from flask import Flask,render_template
+#----------------1.Init app-----------------
+app = Flask(__name__)
+#----------------2. Routers-----------------
+@app.route('/')
+def index():
+    return 'Hello world'
+
+@app.route('/bar')
+def bar():
+    return render_template('bar.html')
+
+@app.route('/foo')
+def foo():
+    return render_template('foo.html')
+
+@app.route('/whereami')
+def whereami():
+    return 'Ghana!'
+
+#@app.route('/foo/<name>')
+#def foo(name):
+  #  return render_template('foo.html', to=name)
+#----------------3. Start Server------------
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1')
