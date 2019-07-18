eef078c1a0faf8e10c7c3bb3366e02925f56fbee
mikelildaps <mikelildaps100@gmail.com>
2019-07-18 14:50:46 +0100

    new fixed one

--- myapp.py
+++ myapp.py
@@ -5,14 +5,14 @@ from flask import Flask,render_template
 app = Flask(__name__)
 #----------------2. Routers-----------------
 
+@app.route('/')
+def foo():
+    return render_template('boot.html')
+
 
 @app.route('/bar')
 def bar():
     return render_template('bar.html')
-
-@app.route('/')
-def foo():
-    return render_template('foo.html')
 
 @app.route('/whereami')
 def whereami():
